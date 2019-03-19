a95526432a5e59f71902e4d367ab8a33096333cf
Nikola Lazarov <nnl160@aubg.edu>
2019-03-19 10:23:19 +0200

    create login route

--- Source/Frontend/src/Router.js
+++ Source/Frontend/src/Router.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
-import Login from './routes/Login';
+import LoginContainer from './containers/LoginContainer';
 import Main from './routes/Main';
 
 const Router = () => (
   <BrowserRouter>
-    <Route path='/' exact component={Login} />
+    <Route path='/' exact component={LoginContainer} />
     <Route path='/main' component={Main} />
   </BrowserRouter>
 );
--- Source/Frontend/src/routes/Login.js
+++ Source/Frontend/src/routes/Login.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { Container, Row, Col, Card, CardBody, Alert } from 'reactstrap';
+import LoginForm from '../components/Login/LoginForm';
+import Loader from '../components/Loader';
+import './Login.scss';
 
-const Login = () => (
+const renderError = (props) => {
+  if (props.isFailed) {
+    return (
+      <Alert color="danger" className="error-alert">
+        {props.error}
+      </Alert>
+    );
+  }
+  return '';
+};
+
+const renderLoader = (props) => {
+  if (props.isLoading) {
+    return <Loader />;
+  }
+}
+
+const Login = (props) => (
   <div id="login">
-    Login route
+    <Container>
+      <Row style={{ height: '100vh' }}>
+        <Col md={{ size: 10, offset: 1 }} className="my-auto" style={{ position: 'relative' }}>
+          { renderError(props) }
+          <Card className="p-2">
+            <CardBody>
+              <Row>
+                <Col>
+                  <LoginForm login={props.login} />
+                  { renderLoader(props) }
+                </Col>
+                <Col>
+                  <h6 className="mb-3">Login instructions</h6>
+                  <p>Please, enter your Username and Password and click the Login button below.</p>
+                  <Row>
+                    <Col md="4"><small className="text-uppercase">Username</small></Col>
+                    <Col md="8"><small>username@studin.aubgin.local</small></Col>
+                  </Row>
+                  <Row>
+                    <Col md="4"><small className="text-uppercase">Password</small></Col>
+                    <Col md="8"><small>•••••••• (domain password)</small></Col>
+                  </Row>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 
