ce673c3ebfaf09d63ff2ebfcc8c9a8e4f444372f
hrist0stoichev <hrist0.stoichev@mail.bg>
2019-04-03 00:51:35 +0300

    Implement stopping a campaign

--- Source/Backend/Web/RoomReservation.Web.Api/Controllers/CampaignsController.cs
+++ Source/Backend/Web/RoomReservation.Web.Api/Controllers/CampaignsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -33,7 +34,7 @@ namespace RoomReservation.Web.Api.Controllers
         [HttpGet("dates")]
         public IActionResult GetPhasesDates()
         {
-            var dates = new List<DateTime>() {
+            var dates = new[] {
                 PhasesProvider.Phase0Start,
                 PhasesProvider.Phase1Start,
                 PhasesProvider.Phase2Start,
@@ -54,51 +55,90 @@ namespace RoomReservation.Web.Api.Controllers
                 return BadRequest(new { error_message = "Cannot start a new campaign until the old one is finished" });
             }
 
-            var rooms = await this.Context.Rooms
-                .Where(r => !r.IsReserved)
-                .ToListAsync();
-
-            rooms.ForEach(r => r.IsMale = null);
-
-            var students = await this.Context.Students
-                .Where(s => s.IsOnCampus)
-                .OrderByDescending(s => s.CreditHours)
-                .ToListAsync();
-
-            students.ForEach(s =>
+            using (var transaction = await Context.Database.BeginTransactionAsync(IsolationLevel.Snapshot))
             {
-                s.PreviousRoomNumber = s.CurrentRoomNumber;
-                s.CurrentRoomNumber = null;
-            });
+                try
+                {
+                    await this.Context.Database.ExecuteSqlCommandAsync("UPDATE Rooms SET IsMale = NULL");
+                    await this.Context.Database.ExecuteSqlCommandAsync("TRUNCATE TABLE Invitations");
+                    await this.Context.Database.ExecuteSqlCommandAsync("UPDATE Students SET PreviousRoomNumber = CurrentRoomNumber, CurrentRoomNumber = NULL");
 
-            double hoursCount = 0;
+                    var students = await this.Context.Students
+                        .Where(s => s.IsOnCampus && !s.IsRA)
+                        .OrderByDescending(s => s.CreditHours)
+                        .ThenBy(s => s.Id)
+                        .ToListAsync();
 
-            for (DateTime i = PhasesProvider.Phase3Start; i < PhasesProvider.Phase3End; i = i.AddHours(1))
+                    int totalMinutesCount = 0;
+
+                    for (DateTime i = (DateTime)model.Phase3Start; i < model.Phase3End; i = i.AddMinutes(1))
+                    {
+                        if (i.Hour < 21 && i.Hour >= 9)
+                        {
+                            totalMinutesCount++;
+                        }
+                    }
+
+                    var minutesPerStudent = totalMinutesCount / students.Count;
+                    int studentIndex = 0;
+
+                    for (DateTime i = (DateTime)model.Phase3Start; i < model.Phase3End; i = i.AddMinutes(minutesPerStudent))
+                    {
+                        if (i.Hour < 21 && i.Hour >= 9)
+                        {
+                            students[studentIndex++].RegistrationTime = i;
+                        }
+                    }
+
+                    await this.Context.SaveChangesAsync();
+                    this.PhasesProvider.ReloadPhases(model);
+
+                    transaction.Commit();
+                    return this.Ok();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return this.BadRequest(new { error_message = "Could not start campaign!" });
+                }
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete]
+        public async Task<IActionResult> StopCampaign()
+        {
+            if (PhasesProvider.CurrentPhase == -1)
             {
-                if (!(i.Hour >= 22 || i.Hour < 10)) hoursCount++;
+                return this.BadRequest(new { error_message = "There is currently no active campaign! " });
             }
 
-            var minutesPerStudent = Math.Floor(hoursCount * 60 / students.Count);
-            int studentIndex = 0;
-
-            for (DateTime i = PhasesProvider.Phase3Start; i < PhasesProvider.Phase3End; i = i.AddMinutes(minutesPerStudent))
+            using (var transaction = await Context.Database.BeginTransactionAsync(IsolationLevel.Snapshot))
             {
-                if (i.Hour >= 22 || i.Hour < 10) continue;
-                students[studentIndex].RegistrationTime = i;
-                studentIndex++;
+                try
+                {
+                    await this.Context.Database.ExecuteSqlCommandAsync("TRUNCATE TABLE Invitations");
+                    await this.Context.Database.ExecuteSqlCommandAsync("UPDATE Students SET RegistrationTime = NULL, CurrentRoomNumber = PreviousRoomNumber, PreviousRoomNumber = NULL");
+                    await this.Context.Database.ExecuteSqlCommandAsync("UPDATE Rooms SET IsMale = s.IsMale FROM Rooms AS r INNER JOIN Students as s on (r.Number = s.PreviousRoomNumber)");
+
+                    this.PhasesProvider.ReloadPhases(new CampaignRequestModel()
+                    {
+                        Phase0Start = DateTime.MinValue,
+                        Phase1Start = DateTime.MinValue,
+                        Phase2Start = DateTime.MinValue,
+                        Phase3Start = DateTime.MinValue,
+                        Phase3End = DateTime.MinValue
+                    });
+
+                    transaction.Commit();
+                    return this.Ok();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return this.BadRequest(new { error_message = "Could not stop campaign!" });
+                }
             }
-
-            // Remove all invitations
-            var invitations = await this.Context.Invitations
-                .ToListAsync();
-
-            this.Context.Invitations.RemoveRange(invitations);
-
-            await this.Context.SaveChangesAsync();
-
-            this.PhasesProvider.ReloadPhases(model);
-
-            return this.Ok();
         }
     }
 }
